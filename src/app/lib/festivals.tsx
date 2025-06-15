35938f4027661d0a388d8548e16918ab94c4c369
ITLimJiaWei <73027268+ITLimJiaWei@users.noreply.github.com>
2025-06-15 18:13:22 +0800

    Add temp test festival data

---
+++ src/app/lib/festivals.tsx
@@ -0,0 +1,21 @@
+// lib/festivals.js
+
+const FESTIVALS = {
+  tanabata: {
+    name: '七夕祭り',
+    location: [38.2682, 140.8694],
+    city: 'Sendai',
+  },
+  gion: {
+    name: '祇園祭',
+    location: [35.0037, 135.7680],
+    city: 'Kyoto',
+  },
+  nebuta: {
+    name: 'ねぶた祭り',
+    location: [40.8246, 140.7400],
+    city: 'Aomori',
+  },
+};
+
+export default FESTIVALS;
